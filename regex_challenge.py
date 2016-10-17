cf15dcd9d507f2fb501abb0c15166f4a4a7c2a23
George Hsin <georgehsin@ucla.edu>
2016-10-17 13:27:37 -0700

    regex first commit

--- regex_challenge.py
+++ regex_challenge.py
@@ -21,5 +21,14 @@ def get_matching_words(regex):
 
     return results
 
-my_expression = r"a"
-print get_matching_words(my_expression)
+print get_matching_words(r'ss') #any words with consecutive 'ss'
+print get_matching_words(r'b.b') #one character between b's
+print get_matching_words(r'b.+b') #one character one or more times between b's
+print get_matching_words(r'b.*b') #one character 0 or more times between b's
+print get_matching_words(r'b.?b') #one character 0 or 1 times between b's
+print get_matching_words(r'a.*e.*i.*o.*u')
+print get_matching_words(r'[a.*e.*i.*o.*u]{2}$')
+print get_matching_words(r'^[regularexpression]+$')
+print get_matching_words(r'^[^regex]+$')
+print get_matching_words(r'(\w)\1.*(\w)\1')
+
