8ae415d99cebcd621192511a6d4600d443543fb3
horseright <977375632@qq.com>
2016-10-17 16:37:55 -0700

    Add files via upload

--- regex_challenge.py
+++ regex_challenge.py
@@ -23,3 +23,33 @@ def get_matching_words(regex):
 
 my_expression = r"a"
 print get_matching_words(my_expression)
+
+my_expression = r"ss"
+print get_matching_words(my_expression)
+
+my_expression = r"b.b."
+print get_matching_words(my_expression)
+
+my_expression = r"b.+b"
+print get_matching_words(my_expression)
+
+my_expression = r"b.*b"
+print get_matching_words(my_expression)
+
+my_expression = r"b.?b"
+print get_matching_words(my_expression)
+
+my_expression = r"a.*e.*i.*o.*u"
+print get_matching_words(my_expression)
+
+my_expression = r"[aeiou]{2}$"
+print get_matching_words(my_expression)
+
+my_expression = r"^[regular expression]*$"
+print get_matching_words(my_expression)
+
+my_expression = r"^[^regex]*$"
+print get_matching_words(my_expression)
+
+my_expression = r"\b\w*(\w{2})\w*\1"
+print get_matching_words(my_expression)
