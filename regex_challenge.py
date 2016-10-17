5d5b69a2d69d65306d787cfa3d6bad00a8ef1140
Nam Tran <namt214@gmail.com>
2016-10-17 13:51:55 -0700

    challange corrected r

--- regex_challenge.py
+++ regex_challenge.py
@@ -22,13 +22,13 @@ def get_matching_words(regex):
     return results
 
 my_expression = r"a"
-print get_matching_words('ss')
-print get_matching_words('b\wb')
-print get_matching_words('b\w+b')
-print get_matching_words('b\w*b')
-print get_matching_words('b.?b')
-print get_matching_words('a[^aeiou]*e[^aeiou]*i[^aeiou]*o[^aeiou]*u')
-print get_matching_words('[aeiou]{2}$')
-print get_matching_words('^[regularexpression]*$')
-print get_matching_words('^[^regex]*$')
+print get_matching_words(r'ss')
+print get_matching_words(r'b\wb')
+print get_matching_words(r'b\w+b')
+print get_matching_words(r'b\w*b')
+print get_matching_words(r'b.?b')
+print get_matching_words(r'a[^aeiou]*e[^aeiou]*i[^aeiou]*o[^aeiou]*u')
+print get_matching_words(r'[aeiou]{2}$')
+print get_matching_words(r'^[regularexpression]*$')
+print get_matching_words(r'^[^regex]*$')
 print get_matching_words(r'^.*(\w{2}).*\1$')
