f7de1e0bcedf419a57451faa2f0831a508bc4e5b
Daniel Li <iammrwiggles@Daniels-MacBook-Pro.local>
2016-12-09 13:44:15 -0800

    Joe and Daniel

--- regex_challenge.py
+++ regex_challenge.py
@@ -23,3 +23,35 @@ def get_matching_words(regex):
 
 my_expression = r"a"
 print get_matching_words(my_expression)
+
+# Group : Joe and Daniel
+
+ex_1= r"ss"
+print get_matching_words(ex_1)
+
+ex_2=r"b.b"
+print get_matching_words(ex_2)
+
+ex_3=r"b.+b"
+print get_matching_words(ex_3)
+
+ex_4=r"b.*b"
+print get_matching_words(ex_4)
+
+ex_5=r"b.?b"
+print get_matching_words(ex_5)
+
+ex_6=r"a.*e.*i.*o.*u"
+print get_matching_words(ex_6)
+
+ex_7=r"[aeiou]{2}$"
+print get_matching_words(ex_7)
+
+ex_8=r"^[regularexpression]+$"
+print get_matching_words(ex_8)
+
+ex_9=r"^[^\p{regex}]+$"
+print get_matching_words(ex_9)
+
+ex_10=r"([a-z])\1.+\1\1"
+print get_matching_words(ex_10)
