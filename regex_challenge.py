ebbecb247126722b44839c707019803fc1e2db75
Stefanie Fluin <sjtrimble@gmail.com>
2016-12-09 12:25:17 -0800

    initial commit, assignment completed by Nolan and Stefanie

--- regex_challenge.py
+++ regex_challenge.py
@@ -21,5 +21,9 @@ def get_matching_words(regex):
 
     return results
 
+<<<<<<< HEAD
+my_expression = r"\w*(\w)\1\w*\1\w*"
+=======
 my_expression = r"a"
+>>>>>>> 25790ef9f8a5bbe8b555c613243e91cdeb4147a8
 print get_matching_words(my_expression)
