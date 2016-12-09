e71bccb8c9dd0e0cfe174d60ff90f5d3b6d0b576
Jen Vat <jennyvat@yahoo.com>
2016-12-09 13:00:59 -0800

    completed assignment in .py

--- regex_challenge.py
+++ regex_challenge.py
@@ -1,3 +1,4 @@
+# Jenny and Kunal
 import re
 
 def get_matching_words(regex):
@@ -21,5 +22,32 @@ def get_matching_words(regex):
 
     return results
 
-my_expression = r"a"
-print get_matching_words(my_expression)
+problem1 = r"ss"
+print get_matching_words(problem1)
+
+problem2 = r"b.b"
+print get_matching_words(problem2)
+
+problem3 = r"b.+b"
+# print get_matching_words(problem3)
+
+problem4 = r"b.*b"
+print get_matching_words(problem4)
+
+problem5 = r"b.?b"
+print get_matching_words(problem5)
+
+problem6 = r"a.*e.*i.*o.*u"
+print get_matching_words(problem6)
+
+problem7 = r"\b.*[aeiou]{2}\b"
+print get_matching_words(problem7)
+
+problem8 = r"^[regular expression]+$"
+print get_matching_words(problem8)
+
+problem9 = r"^[^regex]+$"
+print get_matching_words(problem9)
+
+problem10 = r"\b\w*?(\w{2})\w*?\1\w?\b"
+print get_matching_words(problem10)
